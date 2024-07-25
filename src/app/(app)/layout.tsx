d684710d8d5cfc0bbb017e557f132c1139c48c19
hzeyuan <599012428@qq.com>
2024-07-25 20:34:42 +0800

    first commit

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -82,7 +82,7 @@ const AppLayout = ({ children }) => {
                             <a
                                 target="_blank"
                                 rel="noreferrer"
-                                href="https://github.com/hzeyuan/bookmarks.ai"
+                                href="https://github.com/hzeyuan/bookmarksAI"
                             >
                                 <div className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
                                     <svg viewBox="0 0 438.549 438.549" className="h-4 w-4">
